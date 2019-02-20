a85e4f84f3da0ad9096ca00474e850b8377625e5
ladanov <ladanov_n@mail.ru>
2019-02-20 18:37:45 +0300

    added list and read methods db controller

--- src/controllers/db.ts
+++ src/controllers/db.ts
@@ -1,6 +1,45 @@
 import { Context } from "koa";
+import Database from "../models/database";
 
-export async function list(context: Context) {}
-export async function read(context: Context) {}
-export async function update(context: Context) {}
-export async function remove(context: Context) {}
+export async function list(ctx: Context) {
+  const page = ctx.request.body.page || 1;
+  const itemInList = Number(process.env.ITEM_IN_RESPONSE);
+
+  try {
+    const databases = await Database.find()
+      .limit(itemInList)
+      .skip(page * itemInList)
+      .exec();
+
+    ctx.body = databases;
+  } catch (err) {
+    ctx.status = 400;
+    ctx.body = {
+      error: {
+        type: "Unexpected",
+        message: "Непредвиденная ошибка"
+      }
+    };
+  }
+}
+
+export async function read(ctx: Context) {
+  const id = ctx.params.id;
+
+  try {
+    const database = await Database.findById(id).exec();
+    ctx.body = database;
+  } catch (err) {
+    ctx.status = 400;
+    ctx.body = {
+      error: {
+        type: "Unexpected",
+        message: "Непредвиденная ошибка"
+      }
+    };
+  }
+}
+
+export async function update(ctx: Context) {}
+
+export async function remove(ctx: Context) {}
